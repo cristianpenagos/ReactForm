d97207282a8e5b1303aae3f00edb6862dc2dd41f
PenagozDev <cristian.penagos@udea.edu.co>
2024-04-22 18:34:15 -0500

    Me cambio de PC envio cambios

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-function Profile() {
-  return (
-    <img
-      src="https://i.imgur.com/MK3eW3Am.jpg"
-      alt="Katherine Johnson"
-    />
+
+
+import Gallery from "./Gallery.js";
+import { Profile } from "./Gallery.js";
+
+export default function App(){
+  return(
+    <Profile />
   )
 }
-
-export default function Gallery(){
-  return (
-    <section>
-      <h1>Cientificos Increibles</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
-  )
-}
-
-//
---
+++ src/Gallery.js
@@ -0,0 +1,22 @@
+export function Profile() {
+    return (
+      <img
+        src="https://i.imgur.com/MK3eW3Am.jpg"
+        alt="Katherine Johnson"
+      />
+    )
+  }
+  
+  export default function Gallery(){
+    return (
+      <section>
+        <h1>Cientificos Increibles</h1>
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+      </section>
+    )
+  }
+  
+  //
